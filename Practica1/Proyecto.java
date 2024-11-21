406cfd829f6868947667c4aa9f19ae7f8bdeb1d2
OmegaBrat04 <a2223330174@alumnos.uat.edu.mx>
2024-11-21 16:25:13 -0600

    Init

--- Practica1/Proyecto.java
+++ Practica1/Proyecto.java
@@ -1,6 +1,6 @@
 package Practica1;
     public class Proyecto{
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
+        System.out.println("Hola Mundo como estan?");
     }
 }
